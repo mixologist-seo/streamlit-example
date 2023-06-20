36d320fae304b3ce470fad72edc79e655bd0b41d
mixologist-seo <137155449+mixologist-seo@users.noreply.github.com>
2023-06-20 17:16:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Rank Tracker",page_icon="📈",layout="wide"   )
 
-st.title('Rank Tracker by Yaniss Illoul from Martech with Me')
-st.header("Rank Tracker (Serpstack API Edition)")
-st.markdown("In order to use this app, you will need a **free Serpstack API** key that allows you to use this interface for 100 free calls a month. You can generate your key by [signing up here](http://serpstack.com?utm_source=FirstPromoter&utm_medium=Affiliate&fpr=martechwithme).")
-st.markdown("This interface has been developed by [Yaniss Illoul](https://www.linkedin.com/in/yanissi/) (Feel free to connect!) from [Martech with Me](https://martechwithme.com/?utm_source=ranktracker&utm_medium=streamlit).")
-st.markdown("If you like this project, please consider visiting my website for more Martech tools and tutorials. Don't hesitate to reach out if you have any feature requests or ideas.")
+st.title('Key word checker')
 
 form = st.form(key='rankTrackerForm')
 
